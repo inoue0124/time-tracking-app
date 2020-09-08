2445ba395974f89f04aa5bc92bde394b678d60d2
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2020-09-08 23:36:56 +0900

    定数クラスファイルを追加

---
+++ TimeTracking/AppConst.swift
@@ -0,0 +1,20 @@
+
+final class AppConst {
+    public let CELL_TYPE_TEXT = "text"
+    public let CELL_TYPE_TIME = "time"
+    public let CELL_TYPE_NOTE = "note"
+    public let CELL_TYPE_CHECK = "check"
+
+    public let CELL_TYPE_TEXT_DISPLAY = "テキスト"
+    public let CELL_TYPE_TIME_DISPLAY = "時間"
+    public let CELL_TYPE_NOTE_DISPLAY = "Q&A"
+    public let CELL_TYPE_CHECK_DISPLAY = "チェック"
+
+    public let CELL_WIDTH_SMALL = 50
+    public let CELL_WIDTH_MEDIUM = 100
+    public let CELL_WIDTH_LARGE = 150
+
+    public let CELL_WIDTH_SMALL_DISPLAY = "狭い"
+    public let CELL_WIDTH_MEDIUM_DISPLAY = "普通"
+    public let CELL_WIDTH_LARGE_DISPLAY = "広い"
+}
